147eba5c80ad7a3f6b8df1138f3464d0464fb62b
ChoiWheatley <chltmdgus604@naver.com>
2023-01-27 16:31:56 +0900

    [battle1] 비트셋은 결국 업데이트에서 제곱 시간이 소요된다.

--- on_progress/problem/battle1/solution.hpp
+++ on_progress/problem/battle1/solution.hpp
@@ -41,75 +41,58 @@ auto foreach_true(bitset<MAX_ID> const &bitmap, Consumer const &consumer) {
   }
 }
 
-class SoldierController {
-public:
-  void hire(int mID, int mTeam, int mScore) {
-    m_existence.set(mID);
-    m_bitmap_team[mTeam].set(mID);
-    m_bitmap_score[mScore].set(mID);
-    m_soldiers[mID] = Soldier{mID, mTeam, mScore};
+array<Soldier, MAX_ID> m_soldiers;
+bitset<MAX_ID> m_existence;
+array<bitset<MAX_ID>, MAX_TEAM> m_bitmap_team;
+array<bitset<MAX_ID>, MAX_SCORE> m_bitmap_score;
+
+void init() {
+  m_soldiers.fill(Soldier{});
+  m_existence.reset();
+  m_bitmap_team.fill(bitset<MAX_ID>{});
+  m_bitmap_score.fill(bitset<MAX_ID>{});
+}
+
+void hire(int mID, int mTeam, int mScore) {
+  m_existence.set(mID);
+  m_bitmap_team[mTeam].set(mID);
+  m_bitmap_score[mScore].set(mID);
+  m_soldiers[mID] = Soldier{mID, mTeam, mScore};
+}
+
+void fire(int mID) { m_existence.reset(mID); }
+
+void updateSoldier(int mID, int mScore) {
+  if (!m_existence.test(mID)) {
+    return;
   }
+  // 이전 스코어를 새 스코어에 할당하는 과정 필요.
+  m_bitmap_score[m_soldiers[mID].mScore].reset(mID);
+  m_bitmap_score[mScore].set(mID);
+  // 실제 값 변경
+  m_soldiers[mID].mScore = mScore;
+}
 
-  void fire(int mID) { m_existence.reset(mID); }
+void updateTeam(int mTeam, int mChangeScore) {
+  // mTeam으로 질의, 일치하는 모든 id에 대하여 change score
+  auto const result = m_bitmap_team[mTeam] & m_existence;
+  foreach_true(result, [mChangeScore](int id) {
+    updateSoldier(id, normalize_score(m_soldiers[id].mScore + mChangeScore));
+  });
+}
 
-  void updateSoldier(int mID, int mScore) {
-    if (!m_existence.test(mID)) {
-      return;
-    }
-    // 이전 스코어를 새 스코어에 할당하는 과정 필요.
-    m_bitmap_score[m_soldiers[mID].mScore].reset(mID);
-    m_bitmap_score[mScore].set(mID);
-    // 실제 값 변경
-    m_soldiers[mID].mScore = mScore;
-  }
-
-  void updateTeam(int mTeam, int mChangeScore) {
-    // mTeam으로 질의, 일치하는 모든 id에 대하여 change score
-    auto const result = m_bitmap_team[mTeam] & m_existence;
-    foreach_true(result, [this, mChangeScore](int id) {
-      updateSoldier(id, normalize_score(m_soldiers[id].mScore + mChangeScore));
-    });
-  }
-
-  int bestSoldier(int mTeam) const {
-    auto const result = m_bitmap_team[mTeam] & m_existence;
-    for (int score = MAX_SCORE - 1; score > 0; --score) {
-      auto filtered = result & m_bitmap_score[score];
-      for (int id = MAX_ID - 1; id > 0; --id) {
-        if (filtered.test(id)) {
-          return id;
-        }
+int bestSoldier(int mTeam) {
+  auto const result = m_bitmap_team[mTeam] & m_existence;
+  for (int score = MAX_SCORE - 1; score > 0; --score) {
+    auto filtered = result & m_bitmap_score[score];
+    for (int id = MAX_ID - 1; id > 0; --id) {
+      if (filtered.test(id)) {
+        return id;
       }
     }
-    // not found
-    return 0;
   }
-
-private:
-  array<Soldier, MAX_ID> m_soldiers;
-  bitset<MAX_ID> m_existence;
-  array<bitset<MAX_ID>, MAX_TEAM> m_bitmap_team;
-  array<bitset<MAX_ID>, MAX_SCORE> m_bitmap_score;
-};
-
-static SoldierController sc;
-
-inline void init() { sc = SoldierController(); }
-
-inline void hire(int mID, int mTeam, int mScore) {
-  sc.hire(mID, mTeam, mScore);
+  // not found
+  return 0;
 }
-
-inline void fire(int mID) { sc.fire(mID); }
-
-inline void updateSoldier(int mID, int mScore) {
-  sc.updateSoldier(mID, mScore);
-}
-
-inline void updateTeam(int mTeam, int mChangeScore) {
-  sc.updateTeam(mTeam, mChangeScore);
-}
-
-inline int bestSoldier(int mTeam) { return sc.bestSoldier(mTeam); }
 
 #endif
