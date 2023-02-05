33a505bb78a17783d086b7b01b218261785d071b
ChoiWheatley <chltmdgus604@naver.com>
2023-02-05 22:33:37 +0900

    [algo] lis :: 백트래킹을 사용하여 LIS 수열까지 뽑아내기

--- on_progress/problem/longest_increasing_sequence/solution.hpp
+++ on_progress/problem/longest_increasing_sequence/solution.hpp
@@ -4,14 +4,15 @@ https://www.notion.so/choiwheatley/LIS-d61b26f5619a4ea1b0412155535ec812
 #ifndef SOLUTION
 #define SOLUTION
 
+#include <algorithm>
 #include <array>
+#include <iterator>
 #include <vector>
 using std::array;
 using std::vector;
+constexpr size_t MAX_CACHE = 1000;
 
 namespace lis_with_bin_search {
-
-constexpr size_t MAX_CACHE = 1000;
 /**
 @brief: cache[i] : 길이 i짜리 LIS의 가장 작은 수열.
 cache배열은 값 자체를 갖고있는 것이 아닌, seq의 인덱스를 가지고 있다.
@@ -45,4 +46,61 @@ inline int lis(const vector<int> &seq) {
   return L;
 }
 } // namespace lis_with_bin_search
+
+namespace lis_with_list {
+
+// X[i] : 입력으로 주어진 수열
+// M[k] : 수열 X의 길이 k인 부분수열 중에서 마지막 인덱스의
+// 값이 가장 작은 값을 저장. 값이지 인덱스가 아님.
+// P[i] : X[i]가 바라보고 있는 M 배열의 인덱스
+inline vector<int> lis(const vector<int> &X) {
+  array<size_t, MAX_CACHE + 1> M;
+  array<size_t, MAX_CACHE> P;
+  int L = 0;
+  int newL = 0;
+
+  M.fill(0);
+  P.fill(0);
+  for (size_t i = 0; i < X.size(); ++i) {
+    int lo = 1;
+    int hi = L;
+    int mid = 1;
+    // find lower bound using binary search
+    while (lo <= hi) {
+      mid = (lo + hi) / 2;
+      if (M[mid] < X[i])
+        lo = mid + 1;
+      else
+        hi = mid - 1;
+    }
+    // after binary search, lo gets 1 greater
+    // than L
+
+    // M replace (or append) new value X[i]
+    // and P keeps tracking on M's index
+    newL = lo;
+    M[newL] = X[i];
+    P[i] = newL;
+
+    // when append we have to set hi value
+    if (newL > L)
+      L = newL;
+  }
+
+  // backtracking to get LIS
+  // we only care when M gets appended!
+  vector<int> ret(L);
+  int r = L - 1;
+  for (int i = (int)X.size() - 1; i >= 0; --i) {
+    if (L == P[i]) {
+      ret[r] = X[i];
+      L--;
+      r--;
+    }
+  }
+
+  return ret;
+}
+
+} // namespace lis_with_list
 #endif
--- on_progress/problem/longest_increasing_sequence/tests.cc
+++ on_progress/problem/longest_increasing_sequence/tests.cc
@@ -14,3 +14,17 @@ TEST(Bin, 2) {
   auto answer = 4;
   ASSERT_EQ(answer, submit);
 }
+
+TEST(List, 1) {
+  auto submit = lis_with_list::lis //
+      ({8, 2, 4, 3, 6, 11, 7, 10, 14, 5});
+  auto answer = 6;
+  ASSERT_EQ(answer, submit.size());
+}
+
+TEST(List, 2) {
+  auto submit = lis_with_list::lis //
+      ({2, 10, 6, 14, 1, 9, 15, 13});
+  auto answer = 4;
+  ASSERT_EQ(answer, submit.size());
+}
